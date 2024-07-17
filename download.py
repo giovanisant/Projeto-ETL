e012e945521e05646a746924c359e0d7f2429291
Giovani <giovanisantos2904@gmail.com>
2024-07-16 23:08:00 -0300

    atualizacao

--- download.py
+++ download.py
@@ -2,8 +2,14 @@ import kaggle
 
 #func para baixar o dataset
 def download_dataset():
+    #dataset do kaggle
     dataset_name = 'fronkongames/steam-games-dataset'
-    kaggle.api.dataset_download_files(dataset_name, path='./dados/', unzip = True)
+
+    #caminho do arquivo baixado
+    dataset_path = './data/'
+    
+    kaggle.api.dataset_download_files(dataset_name, path = dataset_path, unzip = True)
+    print(f'Dataset baixado e descompactado em {dataset_path}')
 
 
 
