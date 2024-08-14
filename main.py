0bd4d0dd7d2edfb0bf6d832d189ddb8974787794
Giovani <giovanisantos2904@gmail.com>
2024-08-14 19:05:15 -0300

    atualizacao

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 import os
-import pandas as pd
-from sqlalchemy import create_engine
-import kaggle
+
 from dotenv import load_dotenv
+from connect_postgresql import load_data_to_db
+from kaggle_dataset import download_dataset
+from kaggle_dataset import create_dataframe
 
 #Carregar variáveis de ambiente do arquivo .env
 load_dotenv('envi_variables.env')
 
-#Baixa e descompacta o dataset do Kaggle
-def download_dataset(dataset_name, dataset_path):
-    
-    kaggle.api.dataset_download_files(dataset_name, path = dataset_path, unzip=True)
-    print(f'Dataset baixado e descompactado em {dataset_path}')
 
-#Cria um DataFrame a partir de um arquivo CSV
-def create_dataframe(file_path):
-    
-    try:
-        data = pd.read_csv(file_path)
-        df = pd.DataFrame(data)
-        print("Dataframe criado com sucesso!")
-        return df
-    except Exception as e:
-        print(f"Erro ao criar DataFrame: {e}")
-        return None
-
-#Carrega os dados do DataFrame para uma tabela no banco de dados
-def load_data_to_db(df, table_name, db_uri):
-    
-    try:
-        engine = create_engine(db_uri)
-        df.to_sql(table_name, engine, if_exists='replace', index=False)
-        print("Tabela criada e dados inseridos com sucesso!")
-    except Exception as e:
-        print(f"Erro ao carregar dados no banco de dados: {e}")
 
 def main():
-    #Configurações vindas do arquivo .env
+ 
     dataset_name = os.getenv('DATASET_NAME')
     dataset_path = os.getenv('DATASET_PATH')
     dataset_file = os.getenv('DATASET_FILE')
@@ -56,14 +31,11 @@ def main():
         print("Erro: A variável de ambiente DATABASE_URI não está definida.")
         return
 
-    #Baixar e descompactar o dataset
     download_dataset(dataset_name, dataset_path)
 
-    #Criar DataFrame
     df = create_dataframe(file_path)
 
     if df is not None:
-        #Carregar dados no banco de dados
         load_data_to_db(df, table_name, db_uri)
 
 if __name__ == '__main__':
